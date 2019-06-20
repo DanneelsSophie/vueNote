25768d89116ffd83b78c41a85a88e1529c186e40
Danneels <danneels.s@hotmail.fr>
2019-06-20 19:34:27 +0200

    router

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,12 @@
 <template>
   <div id="app">
-    <ol>
-      <li v-for="todo in todos" v-bind:key="todo.id"  >
-        {{ todo.text }}
-        <button v-on:click="del(todo.id) ">delete a note</button>
-
-      </li>
-    </ol>
-    <input v-model="message" placeholder="modifiez-moi">
-    <p>Your futur todo is : {{ message }}</p>
-    <button v-on:click="add ">Add a note</button>
-
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-
-export default {
-  name: 'app',
-    data () {
-        return {
-            message:'',
-            todos: [
-                { text: 'Apprendre JavaScript', id:0 },
-                { text: 'Apprendre Vue', id:1 },
-                { text: 'Créer quelque chose de génial', id:2 }
-            ],
-            counter: 3
-        }
-    },
-    methods: {
-        add() {
-            this.counter++;
-            this.todos.push({text: this.message,id:this.counter})
-        },
-        del(id) {
-            this.todos = this.todos.filter(todo => todo.id != id);
-        }
-    }
-
-
-
-
-
-
-
-}
-</script>
 
 <style>
 #app {
