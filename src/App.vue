80a749449344650e7ceb6827897e87881909c279
Danneels <danneels.s@hotmail.fr>
2019-06-20 19:43:08 +0200

    Enlever le router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <ol>
+      <li v-for="todo in todos" v-bind:key="todo.id"  >
+        {{ todo.text }}
+        <button v-on:click="del(todo.id) ">delete a note</button>
+
+      </li>
+    </ol>
+    <input v-model="message" placeholder="modifiez-moi">
+    <p>Your futur todo is : {{ message }}</p>
+    <button v-on:click="add ">Add a note</button>
+
   </div>
 </template>
 
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                message:'',
+                todos: [
+                    { text: 'Apprendre JavaScript', id:0 },
+                    { text: 'Apprendre Vue', id:1 },
+                    { text: 'Créer quelque chose de génial', id:2 }
+                ],
+                counter: 3
+            }
+        },
+        methods: {
+            add() {
+                this.counter++;
+                this.todos.push({text: this.message,id:this.counter})
+            },
+            del(id) {
+                this.todos = this.todos.filter(todo => todo.id != id);
+            }
+        }
+    }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
